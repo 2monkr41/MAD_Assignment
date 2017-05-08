01160f02b4e453050dbe4920433d494ae547f805
2monkr41 <2monkr41@solent.ac.uk>
2017-05-08 17:49:56 +0100

    task2

--- app/src/main/java/com/example/a2monkr41/mad_assignment/AddPoiActivity.java
+++ app/src/main/java/com/example/a2monkr41/mad_assignment/AddPoiActivity.java
@@ -5,12 +5,47 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.app.ListActivity;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
 
 public class AddPoiActivity extends Activity{
+
+
 
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_poi_layout);
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener((OnClickListener) this);
+    }
+
+    public void onClick(View view)
+    {
+        Bundle bundle=new Bundle();
+
+        EditText poiName = (EditText)findViewById(R.id.poiname);
+        bundle.putString("com.example.poiname",poiName.getText().toString());
+
+        EditText poiType = (EditText)findViewById(R.id.poitype);
+        bundle.putString("com.example.poitype",poiType.getText().toString());
+
+        EditText poiDesc = (EditText)findViewById(R.id.poidescription);
+        bundle.putString("com.example.poidescription",poiDesc.getText().toString());
+
+
+
+
+
+
+
+        Intent intent = new Intent();
+        intent.putExtras(bundle);
+        setResult(RESULT_OK,intent);
+        finish();
+
     }
 }
